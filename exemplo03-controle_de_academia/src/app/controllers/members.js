3a8d4a738a9ec68f6397f2818800bb1912fc1936
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2020-07-22 19:36:05 -0300

    Finished function instructor of student

--- exemplo03-controle_de_academia/src/app/controllers/members.js
+++ exemplo03-controle_de_academia/src/app/controllers/members.js
@@ -1,14 +1,14 @@
 const { age, date } = require("../../lib/utils")
-const member = require("../models/member")
+const Member = require("../models/member")
 
 module.exports = {
     index(req, res) {
-        member.all(function(members) {
+        Member.all(function(members) {
             return res.render("members/index", { members })
         })
     },
     show(req, res) {
-        member.find(req.params.id, function(member) {
+        Member.find(req.params.id, function(member) {
             if(!member) {
                 return res.send("Member not found")
             }
@@ -19,9 +19,7 @@ module.exports = {
         })
     },
     create(req, res) {
-        member.instructorsSelectOptions(function(options) {
-            
-
+        Member.instructorsSelectOptions(function(options) {
             return res.render("members/create", { instructorOptions: options })
         })
     },
@@ -33,19 +31,21 @@ module.exports = {
             }
         }
 
-        member.create(req.body, function(member) {
+        Member.create(req.body, function(member) {
             return res.redirect(`members/${member.id}`)
         })
     },
     edit(req, res) {
-        member.find(req.params.id, function(member) {
+        Member.find(req.params.id, function(member) {
             if(!member) {
                 return res.send("Member not found")
             }
 
             member.birth = date(member.birth).iso
 
-            return res.render("members/edit", {member})
+            Member.instructorsSelectOptions(function(options) {
+                return res.render("members/edit", { member, instructorOptions: options })
+            })
         })
     },
     put(req, res) {
@@ -56,12 +56,12 @@ module.exports = {
             }
         }
 
-        member.update(req.body, function() {
+        Member.update(req.body, function() {
             return res.redirect(`members/${req.body.id}`)
         })
     },
     delete(req, res) {
-        member.delete(req.body.id, function() {
+        Member.delete(req.body.id, function() {
             return res.redirect(`/members`)
         })
     }
